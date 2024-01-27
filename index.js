7d785d6f6d0c8b9008d3bee17589d29d814f5308
VeztWare <117451997+VeztWare@users.noreply.github.com>
2024-01-27 19:05:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="refresh" content="0;url=https://www.youtube.com/your-channel"> <!-- Replace 'your-channel' with your actual YouTube channel URL -->
+    <meta http-equiv="refresh" content="0;url=dsc.gg/veztontop"> <!-- Replace 'your-channel' with your actual YouTube channel URL -->
     <title>Redirecting to YouTube</title>
 </head>
 </html>
