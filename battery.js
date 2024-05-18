82e142413f4f47ee0e4c6e4b6da1667ac85676a4
VeztWare <117451997+VeztWare@users.noreply.github.com>
2024-05-18 20:19:41 +0100

    Delete battery.js

--- battery.js
+++
@@ -1,7 +0,0 @@
-if ('getBattery' in navigator) {
-    navigator.getBattery().then(function(battery) {
-        document.body.innerText = `${Math.floor(battery.level * 100)}%`;
-    });
-} else {
-    document.body.innerText = 'Battery API not supported';
-}
