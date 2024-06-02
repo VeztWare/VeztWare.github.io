3e74d9d23053b7814f0a275556174f504a8abfe3
VeztWare <117451997+VeztWare@users.noreply.github.com>
2024-06-02 02:17:31 +0100

    Delete netlify/function directory

--- netlify/function/ip.js
+++
@@ -1,7 +0,0 @@
-exports.handler = async (event, context) => {
-  const clientIp = event.headers['x-nf-client-connection-ip'] || event.headers['client-ip'] || 'IP not found';
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ ip: clientIp }),
-  };
-};
