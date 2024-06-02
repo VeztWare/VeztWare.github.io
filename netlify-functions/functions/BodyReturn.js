741f5dbd2858fae7101107693d033b7c88138efc
VeztWare <117451997+VeztWare@users.noreply.github.com>
2024-06-02 01:55:59 +0100

    Update BodyReturn.js

--- netlify-functions/functions/BodyReturn.js
+++ netlify-functions/functions/BodyReturn.js
@@ -1,6 +1,9 @@
-export default async (req: Request, context: Context) => {
-  if (req.method === 'GET') {
-    return new Response("Hello, this is a GET request response!");
-  }
-  return new Response("Sorry, this function only responds to GET requests.", { status: 400 });
-};
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: "Hello, world!",
+    headers: {
+      'Content-Type': 'text/plain'
+    }
+  };
+}
